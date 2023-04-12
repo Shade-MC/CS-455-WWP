5aad49b8c6845bcacd513df3cad5811c5a12546e
Eric Curtland <554scout@gmail.com>
2023-04-12 14:38:18 -0700

    Initial Project setup and math simulation for ball

---
+++ app/src/main/java/com/example/cs_455_wwp/Ball.java
@@ -0,0 +1,97 @@
+package com.example.cs_455_wwp;
+
+import androidx.annotation.NonNull;
+
+import com.google.ar.sceneform.math.Vector3;
+
+
+public class Ball {
+    private Vector3 position;
+    private Vector3 speed;
+    private Vector3 acceleration;
+    private static final double EARTH_RADIUS = 6378137.0; // in meters
+
+    public Ball(){
+        this.position =  new Vector3(45, 45, 0);
+        this.speed = new Vector3(100, 100, 1000);
+        this.acceleration = new Vector3(0,0,(float) -9.8);
+    }
+
+    public Ball(Vector3 position, Vector3 speed, Vector3 acceleration){
+        this.position = position;
+        this.speed = speed;
+        this.acceleration = acceleration;
+    }
+
+    public void updatePosition(double deltaTime){
+//        System.out.println("deltaTime: " + deltaTime);
+        // Calculate the speed in meters per hour
+        Vector3 speedInMetersPerHour = this.speed.scaled(3600);
+        speedInMetersPerHour.z = 0;
+
+        // Calculate the distance traveled in meters
+        double distanceTraveled = speedInMetersPerHour.length()  * deltaTime;
+
+        // Calculate the angle of travel in radians
+        double angleOfTravel = Math.atan2(this.speed.y, this.speed.x);
+
+        // Calculate the change in latitude and longitude
+        double deltaLatitude = Math.toDegrees(distanceTraveled * Math.cos(angleOfTravel) /
+                EARTH_RADIUS);
+        double deltaLongitude = Math.toDegrees(distanceTraveled * Math.sin(angleOfTravel) /
+                (EARTH_RADIUS * Math.cos(Math.toRadians(0))));
+
+        // Calculate the change in altitude
+        double deltaAltitude = this.speed.z * deltaTime;
+
+        this.position = Vector3.add(this.position, new Vector3((float) deltaLatitude,
+                (float) deltaLongitude, (float) deltaAltitude));
+
+        if (Math.abs(this.position.x) > 180){
+            float over = this.position.x - (180 * Math.signum(this.position.x));
+            this.position.x *= -1;
+            this.position.x += over;
+        }
+        if (Math.abs(this.position.y) > 90){
+            float over = this.position.y - (90 * Math.signum(this.position.y));
+            this.speed.y *= -1;
+            this.position.y = (90 * Math.signum(this.position.y)) - over;
+        }
+
+        if (this.position.z < 0) this.position.z = 0;
+
+        this.speed = Vector3.add(this.speed, this.acceleration.scaled((float) deltaTime));
+
+    }
+
+    public Vector3 getSpeed() {
+        return speed;
+    }
+
+    public void setSpeed(Vector3 speed) {
+        this.speed = speed;
+    }
+
+    public Vector3 getAcceleration() {
+        return acceleration;
+    }
+
+    public void setAcceleration(Vector3 acceleration) {
+        this.acceleration = acceleration;
+    }
+
+    public Vector3 getPosition() {
+        return position;
+    }
+
+    public void setPosition(Vector3 position) {
+        this.position = position;
+    }
+
+    @NonNull
+    public String toString(){
+        return  "Position: " + this.position.toString() + "\n" +
+                "Speed: " + this.speed.toString() + "\n" +
+                "Acceleration: " + this.acceleration.toString();
+    }
+}
---
+++ app/src/main/java/com/example/cs_455_wwp/MainActivity.java
@@ -0,0 +1,105 @@
+package com.example.cs_455_wwp;
+
+import android.annotation.SuppressLint;
+import android.os.Bundle;
+import android.view.SurfaceHolder;
+import android.view.SurfaceView;
+import android.widget.TextView;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class MainActivity extends AppCompatActivity implements SurfaceHolder.Callback {
+
+    private TextView ballStats;
+    private GameThread gameThread;
+    private boolean isRunning;
+
+    @SuppressLint("MissingInflatedId")
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        // Set up the surface view
+        SurfaceView surfaceView = findViewById(R.id.gameView);
+        surfaceView.getHolder().addCallback(this);
+
+        this.ballStats = findViewById(R.id.ballStats);
+    }
+
+
+    @Override
+    public void surfaceCreated(SurfaceHolder holder) {
+        // Start the game thread when the surface is created
+        gameThread = new GameThread();
+        gameThread.setSurfaceHolder(holder);
+        gameThread.setRunning(true);
+        gameThread.start();
+    }
+
+    @Override
+    public void surfaceChanged(SurfaceHolder holder, int format, int width, int height) {
+        // Handle surface changes if needed
+    }
+
+    @Override
+    public void surfaceDestroyed(SurfaceHolder holder) {
+        // Stop the game thread when the surface is destroyed
+        boolean retry = true;
+        gameThread.setRunning(false);
+        while (retry) {
+            try {
+                gameThread.join();
+                retry = false;
+            } catch (InterruptedException e) {
+                // Retry
+            }
+        }
+    }
+
+    private class GameThread extends Thread {
+        private final Ball gameBall;
+        private boolean isRunning;
+        private long lastFrameTime;
+
+        public GameThread() {
+            this.gameBall = new Ball();
+            lastFrameTime = System.nanoTime();
+        }
+
+        public void setSurfaceHolder(SurfaceHolder surfaceHolder) {
+        }
+
+        public void setRunning(boolean isRunning) {
+            this.isRunning = isRunning;
+        }
+
+        @Override
+        public void run() {
+            while (isRunning) {
+                long currentFrameTime = System.nanoTime();
+                double deltaTime = (currentFrameTime - lastFrameTime) / 1e9;
+                lastFrameTime = currentFrameTime;
+                // Update the game state here
+                // For example, calculate the new position based on the speed vector
+
+                // Lock the canvas and draw the game objects here
+                // For example, use surfaceHolder.lockCanvas() to get a Canvas object
+
+                // Unlock the canvas and show the changes here
+                // For example, use surfaceHolder.unlockCanvasAndPost(Canvas) to show the changes
+                this.gameBall.updatePosition(deltaTime);
+                // Update the speed text view here
+
+                runOnUiThread(() -> ballStats.setText(gameBall.toString()));
+
+                // Sleep for a short period of time to control the frame rate
+                try {
+                    sleep(500);
+                } catch (InterruptedException e) {
+                    // Do nothing
+                }
+            }
+        }
+    }
+}
