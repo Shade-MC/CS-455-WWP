02ffa54fbbbae04524216eb069cc4e8758d27718
Emily Lim <lim.emi@outlook.com>
2023-04-23 20:24:09 -0700

    Moved ball component to game screen

--- app/src/main/java/com/example/cs_455_wwp/GameActivity.java
+++ app/src/main/java/com/example/cs_455_wwp/GameActivity.java
@@ -1,9 +1,5 @@
 package com.example.cs_455_wwp;
 
-import androidx.annotation.NonNull;
-import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.app.ActivityCompat;
-
 import android.Manifest;
 import android.annotation.SuppressLint;
 import android.content.Intent;
@@ -12,12 +8,17 @@ import android.location.Address;
 import android.location.Geocoder;
 import android.location.Location;
 import android.os.Bundle;
+import android.view.SurfaceHolder;
+import android.view.SurfaceView;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+
 import com.example.cs_455_wwp.databinding.ActivityGameBinding;
-import com.example.cs_455_wwp.databinding.ActivityMainBinding;
 import com.google.android.gms.location.FusedLocationProviderClient;
 import com.google.android.gms.location.LocationServices;
 import com.google.android.gms.tasks.OnCompleteListener;
@@ -29,7 +30,7 @@ import java.io.IOException;
 import java.util.List;
 import java.util.Locale;
 
-public class GameActivity extends AppCompatActivity {
+public class GameActivity extends AppCompatActivity implements SurfaceHolder.Callback {
 
     // view binding
     private ActivityGameBinding binding;
@@ -41,6 +42,11 @@ public class GameActivity extends AppCompatActivity {
     TextView gpsText;
     FusedLocationProviderClient fusedLocationProviderClient;
 
+    private TextView ballStats;
+    private GameThread gameThread;
+    private boolean isRunning;
+
+    @SuppressLint("MissingInflatedId")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -81,6 +87,12 @@ public class GameActivity extends AppCompatActivity {
                 }
             }
         });
+
+        // Set up the surface view
+        SurfaceView surfaceView = findViewById(R.id.gameView);
+        surfaceView.getHolder().addCallback(this);
+
+        this.ballStats = findViewById(R.id.ballStats);
     }
 
     private void checkUser() {
@@ -128,5 +140,80 @@ public class GameActivity extends AppCompatActivity {
                 }
             }
         });
+    }
+
+    @Override
+    public void surfaceCreated(SurfaceHolder holder) {
+        // Start the game thread when the surface is created
+        gameThread = new GameThread();
+        gameThread.setSurfaceHolder(holder);
+        gameThread.setRunning(true);
+        gameThread.start();
+    }
+
+    @Override
+    public void surfaceChanged(SurfaceHolder holder, int format, int width, int height) {
+        // Handle surface changes if needed
+    }
+
+    @Override
+    public void surfaceDestroyed(SurfaceHolder holder) {
+        // Stop the game thread when the surface is destroyed
+        boolean retry = true;
+        gameThread.setRunning(false);
+        while (retry) {
+            try {
+                gameThread.join();
+                retry = false;
+            } catch (InterruptedException e) {
+                // Retry
+            }
+        }
+    }
+
+    private class GameThread extends Thread {
+        private final Ball gameBall;
+        private boolean isRunning;
+        private long lastFrameTime;
+
+        public GameThread() {
+            this.gameBall = new Ball();
+            lastFrameTime = System.nanoTime();
+        }
+
+        public void setSurfaceHolder(SurfaceHolder surfaceHolder) {
+        }
+
+        public void setRunning(boolean isRunning) {
+            this.isRunning = isRunning;
+        }
+
+        @Override
+        public void run() {
+            while (isRunning) {
+                long currentFrameTime = System.nanoTime();
+                double deltaTime = (currentFrameTime - lastFrameTime) / 1e9;
+                lastFrameTime = currentFrameTime;
+                // Update the game state here
+                // For example, calculate the new position based on the speed vector
+
+                // Lock the canvas and draw the game objects here
+                // For example, use surfaceHolder.lockCanvas() to get a Canvas object
+
+                // Unlock the canvas and show the changes here
+                // For example, use surfaceHolder.unlockCanvasAndPost(Canvas) to show the changes
+                this.gameBall.updatePosition(deltaTime);
+                // Update the speed text view here
+
+                runOnUiThread(() -> ballStats.setText(gameBall.toString()));
+
+                // Sleep for a short period of time to control the frame rate
+                try {
+                    sleep(500);
+                } catch (InterruptedException e) {
+                    // Do nothing
+                }
+            }
+        }
     }
 }
--- app/src/main/java/com/example/cs_455_wwp/MainActivity.java
+++ app/src/main/java/com/example/cs_455_wwp/MainActivity.java
@@ -1,27 +1,14 @@
 package com.example.cs_455_wwp;
 
-import android.Manifest;
-import android.annotation.SuppressLint;
 import android.content.Intent;
-import android.content.pm.PackageManager;
 import android.os.Bundle;
 import android.util.Log;
-import android.view.SurfaceHolder;
-import android.view.SurfaceView;
 import android.view.View;
-import android.widget.Button;
-import android.widget.TextView;
-
-import androidx.annotation.Nullable;
-import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.app.ActivityCompat;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
-
-import android.location.Address;
-import android.location.Geocoder;
-import android.location.Location;
-import android.widget.Toast;
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
 
 import com.example.cs_455_wwp.databinding.ActivityMainBinding;
 import com.google.android.gms.auth.api.signin.GoogleSignIn;
@@ -29,9 +16,6 @@ import com.google.android.gms.auth.api.signin.GoogleSignInAccount;
 import com.google.android.gms.auth.api.signin.GoogleSignInClient;
 import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
 import com.google.android.gms.common.api.ApiException;
-import com.google.android.gms.location.FusedLocationProviderClient;
-import com.google.android.gms.location.LocationServices;
-import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.OnFailureListener;
 import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.android.gms.tasks.Task;
@@ -41,15 +25,7 @@ import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.auth.GoogleAuthProvider;
 
-import java.io.IOException;
-import java.util.List;
-import java.util.Locale;
-
-public class MainActivity extends AppCompatActivity implements SurfaceHolder.Callback {
-
-    private TextView ballStats;
-    private GameThread gameThread;
-    private boolean isRunning;
+public class MainActivity extends AppCompatActivity {
 
     // view binding
     private ActivityMainBinding binding;
@@ -61,17 +37,10 @@ public class MainActivity extends AppCompatActivity implements SurfaceHolder.Cal
 
     private static final String TAG = "GOOGLE_SIGN_IN_TAG";
 
-    @SuppressLint("MissingInflatedId")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
-        // Set up the surface view
-        SurfaceView surfaceView = findViewById(R.id.gameView);
-        surfaceView.getHolder().addCallback(this);
-
-        this.ballStats = findViewById(R.id.ballStats);
 
         // binding
         binding = ActivityMainBinding.inflate(getLayoutInflater());
@@ -166,81 +135,6 @@ public class MainActivity extends AppCompatActivity implements SurfaceHolder.Cal
                         Log.d(TAG, "onFailure: Login Failed. " + e.getMessage());
                     }
                 });
-    }
-
-    @Override
-    public void surfaceCreated(SurfaceHolder holder) {
-        // Start the game thread when the surface is created
-        gameThread = new GameThread();
-        gameThread.setSurfaceHolder(holder);
-        gameThread.setRunning(true);
-        gameThread.start();
-    }
-
-    @Override
-    public void surfaceChanged(SurfaceHolder holder, int format, int width, int height) {
-        // Handle surface changes if needed
-    }
-
-    @Override
-    public void surfaceDestroyed(SurfaceHolder holder) {
-        // Stop the game thread when the surface is destroyed
-        boolean retry = true;
-        gameThread.setRunning(false);
-        while (retry) {
-            try {
-                gameThread.join();
-                retry = false;
-            } catch (InterruptedException e) {
-                // Retry
-            }
-        }
-    }
-
-    private class GameThread extends Thread {
-        private final Ball gameBall;
-        private boolean isRunning;
-        private long lastFrameTime;
-
-        public GameThread() {
-            this.gameBall = new Ball();
-            lastFrameTime = System.nanoTime();
-        }
-
-        public void setSurfaceHolder(SurfaceHolder surfaceHolder) {
-        }
-
-        public void setRunning(boolean isRunning) {
-            this.isRunning = isRunning;
-        }
-
-        @Override
-        public void run() {
-            while (isRunning) {
-                long currentFrameTime = System.nanoTime();
-                double deltaTime = (currentFrameTime - lastFrameTime) / 1e9;
-                lastFrameTime = currentFrameTime;
-                // Update the game state here
-                // For example, calculate the new position based on the speed vector
-
-                // Lock the canvas and draw the game objects here
-                // For example, use surfaceHolder.lockCanvas() to get a Canvas object
-
-                // Unlock the canvas and show the changes here
-                // For example, use surfaceHolder.unlockCanvasAndPost(Canvas) to show the changes
-                this.gameBall.updatePosition(deltaTime);
-                // Update the speed text view here
-
-                runOnUiThread(() -> ballStats.setText(gameBall.toString()));
-
-                // Sleep for a short period of time to control the frame rate
-                try {
-                    sleep(500);
-                } catch (InterruptedException e) {
-                    // Do nothing
-                }
-            }
-        }
     }
 
 }
